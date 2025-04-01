0485b0b53ceea0eaa625b52ff16879777f8d51dc
Oksana T. <pochtoviy-yashik@mail.ru>
2025-04-01 15:26:16 +0400

    brand name const.js

--- src/const.js
+++ src/const.js
@@ -1,7 +1,7 @@
 import { phoneFormat } from '@/js/utils/numbers.format';
 import { LAST_DAY, MONTH, YEAR } from '@/js/utils/date';
 // Название сайта по умолчанию
-export const SITE_NAME = 'Название сайта';
+export const SITE_NAME = 'WEY';
 // Юр лицо
 export const LEGAL_ENTITY = 'ООО «Юридическое название»';
 // ИНН
